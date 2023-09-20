45b875bc4f5ec5a4ce4fee74194825b31f8f017c
Ori Barda <oribarda10@gmail.com>
2023-09-20 09:39:30 +0300

    third

--- src/General_Components/Footer.jsx
+++ src/General_Components/Footer.jsx
@@ -1,21 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import "./Footer.css"
+import React, { useState, useEffect } from "react";
+import profile1 from "./img/profile1.jpg";
 function Footer() {
   const comments = [
-    'Great product! I love it!',
-    'Very user-friendly and helpful.',
-    'I recommend this to everyone.',
-    'Outstanding customer support!',
-    'An essential tool for productivity.',
-    'Excellent value for the price.',
-    'I cant imagine life without it!',
+    [
+      <div className="comment-container">
+        <span>
+          <img className="profile-image" src={profile1} alt="" />
+          Eli Miller|3 Years User|
+        </span>
+      </div>,
+      "Great product! I love it!",
+    ],
+    [
+      <div className="comment-container">
+        <span>
+          <img className="profile-image" src={profile1} alt="" />
+          Elazzar Bruker|1 Year User|
+        </span>
+      </div>,
+      "Very user-friendly and helpful.",
+    ],
+    [
+      <div className="comment-container">
+        <span>
+          <img className="profile-image" src={profile1} alt="" />
+          Shlomi|6 Years User|
+        </span>
+      </div>,
+      "I recommend this to everyone.",
+    ],
+    [
+      <div className="comment-container">
+        <span>
+          <img className="profile-image" src={profile1} alt="" />
+          Yehonatan Ben-Ezra|4 Years User|
+        </span>
+      </div>,
+      "Outstanding customer support!",
+    ],
+    [
+      <div className="comment-container">
+        <span>
+          <img className="profile-image" src={profile1} alt="" />
+          Or Reuben|2 Years User|
+        </span>
+      </div>,
+      "An essential tool for productivity.",
+    ],
+    [
+      <div className="comment-container">
+        <span>
+          <img className="profile-image" src={profile1} alt="" />
+          Anna Baruch|9 Months User|
+        </span>
+      </div>,
+      "Excellent value for the price.",
+    ],
+    [
+      <div className="comment-container">
+        <span>
+          <img className="profile-image" src={profile1} alt="" />
+          Eli Miller|3 Years User|
+        </span>
+      </div>,
+      "I cant imagine life without it!",
+    ],
   ];
 
   const [currentCommentIndex, setCurrentCommentIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentCommentIndex((prevIndex) => getRandomIndex(prevIndex, comments.length));
+      setCurrentCommentIndex((prevIndex) =>
+        getRandomIndex(prevIndex, comments.length)
+      );
     }, 5000);
 
     return () => clearInterval(interval);
@@ -32,11 +90,11 @@ function Footer() {
 
   return (
     <>
-      <h1>Our Users Recommend</h1>
-      <div id='comments'>
-        <p className='comment-animation'>
-          {comments[currentCommentIndex]}
-        </p>
+      <div id="reco-container">
+        <h1 id="heading">Our Users Recommend</h1>
+        <div id="comments">
+          <p className="comment-animation">{comments[currentCommentIndex]}</p>
+        </div>
       </div>
     </>
   );
--- src/comp/Sign-In-Up/LogIn.jsx
+++ src/comp/Sign-In-Up/LogIn.jsx
@@ -2,10 +2,10 @@ import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import users from "../../UserInfo.json";
 import { UserContext } from "../../General_Components/Context";
-import "./LogIn.css"
-import Footer from "../../General_Components/Footer"
+import "./LogIn.css";
+import Footer from "../../General_Components/Footer";
 function LogIn() {
-  const {user, setUser} = useContext(UserContext) 
+  const { user, setUser } = useContext(UserContext);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
@@ -21,28 +21,30 @@ function LogIn() {
     } else {
       alert("Username not found. Please sign up.");
     }
-  }
+  };
   return (
     <div id="container">
-      <h2>Log In</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        minLength="8"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleSignIn}>Log In</button>
-      <p>
-        Don't have an account? <Link to="/signup">Sign Up</Link>
-      </p>
-      <Footer/>
+      <div id="login-container">
+        <h2 id="login-header">Log In</h2>
+        <input className="login-input"
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          minLength="8"
+        />
+        <input className="login-input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button id="login-button" onClick={handleSignIn}>Log In</button>
+        <p id="no-account">
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </p>
+      </div>
+      <Footer />
     </div>
   );
 }
